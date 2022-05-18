3e8ae8f0897b39c8cfa412a269a8ed1630c7c0a2
ibraouh <ibrahim.raouh@gmail.com>
2022-05-18 13:18:00 -0400

    Python file

---
+++ logisticRegression.py
@@ -0,0 +1,53 @@
+def weightInitialization(n_features):
+    w = np.zeros((1,n_features))
+    b = 0
+    return w,b
+def sigmoid_activation(result):
+    final_result = 1/(1+np.exp(-result))
+    return final_result
+def model_optimize(w, b, X, Y):
+    m = X.shape[0]
+    
+    #Prediction
+    final_result = sigmoid_activation(np.dot(w,X.T)+b)
+    Y_T = Y.T
+    cost = (-1/m)*(np.sum((Y_T*np.log(final_result)) + ((1-Y_T)*(np.log(1-final_result)))))
+    #
+    
+    #Gradient calculation
+    dw = (1/m)*(np.dot(X.T, (final_result-Y.T).T))
+    db = (1/m)*(np.sum(final_result-Y.T))
+    
+    grads = {"dw": dw, "db": db}
+    
+    return grads, cost
+def model_predict(w, b, X, Y, learning_rate, no_iterations):
+    costs = []
+    for i in range(no_iterations):
+        #
+        grads, cost = model_optimize(w,b,X,Y)
+        #
+        dw = grads["dw"]
+        db = grads["db"]
+        #weight update
+        w = w - (learning_rate * (dw.T))
+        b = b - (learning_rate * db)
+        #
+        
+        if (i % 100 == 0):
+            costs.append(cost)
+            #print("Cost after %i iteration is %f" %(i, cost))
+    
+    #final parameters
+    coeff = {"w": w, "b": b}
+    gradient = {"dw": dw, "db": db}
+    
+    return coeff, gradient, costs
+def predict(final_pred, m):
+    y_pred = np.zeros((1,m))
+    for i in range(final_pred.shape[1]):
+        if final_pred[0][i] > 0.5:
+            y_pred[0][i] = 1
+    return y_pred
+
+
